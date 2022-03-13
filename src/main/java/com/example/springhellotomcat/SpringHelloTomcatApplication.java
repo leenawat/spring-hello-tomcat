dfa813036f3f8a23d1deecaf7d846bd356845aca
Leenawat Papahom <leenawat.pa@gmail.com>
2022-03-13 09:53:01 +0700

    init project

---
+++ src/main/java/com/example/springhellotomcat/SpringHelloTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhellotomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloTomcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springhellotomcat/SpringHelloTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhellotomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
